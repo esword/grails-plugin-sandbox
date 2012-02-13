c0f860c7f26edd8db3537d2b93ce0da16126fa1f
Eric Sword <eric.r.sword@saic.com>
2012-02-13 01:21:29 -0500

    clean up a bit, though still can't figure out how to limit melody plugin by role

--- grails-app/conf/SecurityConfig.groovy
+++ grails-app/conf/SecurityConfig.groovy
@@ -21,40 +21,33 @@ grails.plugins.springsecurity.authority.className = 'grails.plugins.sandbox.auth
 //http://swordsystems.com/2011/12/12/auto-create-user-domain-object-with-spring-security/
 grails.plugins.springsecurity.useSecurityEventListener = true
 
-//make ldap explicitly inactive by default.  Need to do this because the default is active since we include
-//the ldap plugin
-grails.plugins.springsecurity.ldap.active = false
-grails.plugins.springsecurity.cas.active = false
-//grails.plugins.springsecurity.active = false
-
 grails.plugins.springsecurity.securityConfigType = SecurityConfigType.InterceptUrlMap
 grails.plugins.springsecurity.interceptUrlMap = [
-        '/monitoring*/**': ["hasRole( 'ROLE_ADMIN' )"],
-        '/securityInfo*/**': ["hasRole( 'ROLE_ADMIN' )"],
+        //'/monitoring': ["hasRole( 'ROLE_ADMIN' )", 'IS_AUTHENTICATED_FULLY'],
+        '/monitoring': ['ROLE_ADMIN'],
+        '/monitoring/**': ['ROLE_ADMIN'],
+        //'dbconsole': ['ROLE_ADMIN'],
+        'dbconsole*/**': ['ROLE_ADMIN'],
 
+        '/securityInfo*/**': ['ROLE_ADMIN'],
+
+        '/': ['permitAll'],
         '/index': ['permitAll'],
-        '/main': ['IS_AUTHENTICATED_FULLY'],
-        '/book/**': ['IS_AUTHENTICATED_FULLY'],
-        '/author/**': ['IS_AUTHENTICATED_FULLY'],
+
+        //'/book/**': ['ROLE_ADMIN'],
+        //'/author/**': ['IS_AUTHENTICATED_FULLY'],
 
         '/login/*': ['permitAll'],
         '/static/**': ['permitAll'],
-        '/js/*': ['permitAll'],
-        '/js/processing/**': ['permitAll'],
-        '/images/**': ['permitAll'],
-        '/grails-errorhandler/**': ['permitAll'],
-        '/startPage': ['permitAll'],
         '/error': ['permitAll'],
-        '/logout': ['permitAll'],
-        '/public': ['permitAll'],
-        'dbConsole/**': ['permitAll'],
+        '/logout/*': ['permitAll'],
 
-        //"/**": ['IS_AUTHENTICATED_FULLY']
-        "/**": ['permitAll']
+        "/**": ['IS_AUTHENTICATED_FULLY']
+        //"/**": ['permitAll']
 ]
 
 environments {
     production {
-        grails.plugins.springsecurity.interceptUrlMap.'dbConsole/**' = ['IS_AUTHENTICATED_FULLY']
+        grails.plugins.springsecurity.interceptUrlMap.'dbconsole/**' = ['IS_AUTHENTICATED_FULLY']
     }
 }

eed698ea98d103edcaadb7a6d2fffe952d77db61
Eric Sword <eric.r.sword@saic.com>
2012-02-13 14:11:56 -0500

    demo build-test-data basic usage

--- test/unit/grails/plugins/sandbox/AuthorTests.groovy
+++ test/unit/grails/plugins/sandbox/AuthorTests.groovy
@@ -4,14 +4,18 @@ package grails.plugins.sandbox
 
 import grails.test.mixin.*
 import org.junit.*
+import grails.buildtestdata.mixin.Build
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Author)
+@Build(Author)
 class AuthorTests {
 
-    void testSomething() {
-       println "Implement me"
+    void testEquals() {
+        def a1 = Author.build()
+        def a2 = new Author(firstName: a1.firstName, lastName: a1.lastName, dob: a1.dob)
+        assert a1 == a2
     }
 }
--- test/unit/grails/plugins/sandbox/BookTests.groovy
+++ test/unit/grails/plugins/sandbox/BookTests.groovy
@@ -4,14 +4,21 @@ package grails.plugins.sandbox
 
 import grails.test.mixin.*
 import org.junit.*
+import grails.buildtestdata.mixin.Build
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Book)
+@Build([Book, Author])
 class BookTests {
 
-    void testSomething() {
-        println "Implement me"
+    void testEquals() {
+        def b1 = Book.build()
+        println b1.authors
+        def b2 = new Book(title: b1.title, isbn: b1.isbn, pages: b1.pages, authors:([].addAll(b1.authors)))
+        assert b1 == b2
+        b2.title = 'Some other title'
+        assert b1 != b2
     }
 }

a83f460c4f941cdcf8fd16ccef3d0a7223c50b93
Eric Sword <eric.r.sword@saic.com>
2012-02-13 14:36:05 -0500

    proper name for systemUser

--- fixtures/SecurityFixture.groovy
+++ fixtures/SecurityFixture.groovy
@@ -10,15 +10,15 @@ fixture {
         adminRole(AuthRole, authority: 'ROLE_ADMIN')
 
     if (!LibraryUser.findByUsername(LibraryUser.DEFAULT_USERNAME))
-        sharingUser(LibraryUser, username: LibraryUser.DEFAULT_USERNAME, name: LibraryUser.DEFAULT_NAME, password: 'password', enabled: true)
+        systemUser(LibraryUser, username: LibraryUser.DEFAULT_USERNAME, name: LibraryUser.DEFAULT_NAME, password: 'password', enabled: true)
 }
 
 post {
-    def sharingUser = LibraryUser.findByUsername(LibraryUser.DEFAULT_USERNAME)
+    def systemUser = LibraryUser.findByUsername(LibraryUser.DEFAULT_USERNAME)
     def adminRole = AuthRole.findByAuthority('ROLE_ADMIN')
     def userRole = AuthRole.findByAuthority('ROLE_USER')
-    if (!UserRoleMapper.findByUserAndRole(sharingUser, adminRole))
-        UserRoleMapper.create sharingUser, adminRole
-    if (!UserRoleMapper.findByUserAndRole(sharingUser, userRole))
-        UserRoleMapper.create sharingUser, userRole
+    if (!UserRoleMapper.findByUserAndRole(systemUser, adminRole))
+        UserRoleMapper.create systemUser, adminRole
+    if (!UserRoleMapper.findByUserAndRole(systemUser, userRole))
+        UserRoleMapper.create systemUser, userRole
 }
